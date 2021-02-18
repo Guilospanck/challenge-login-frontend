33b4f2e313e83b4a1ecad241823f541d04f439a3
guilospanck <guilherme.rp2014@gmail.com>
2021-02-18 16:25:33 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Hello world
+        </h1>
+      </main>
+
+    </div>
+  )
+};
+
+export default Home;
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+const theme = {
+  colors: {
+    background: '#E5E5E5',
+    text: '#383E71',
+    primary: '#8257e6'
+  }
+};
+
+export default theme;
